4ca575137c325c043bd542da26ba22b69e4af65a
zainsci <zainansari08@gmail.com>
2020-10-01 19:58:05 +0500

    Add HTML to PublicHome

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import PublicHome from "./Pages/PublicHome";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PublicHome />
+    </div>
+  );
 }
 
 export default App;
