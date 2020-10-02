0378712537d34bb49d619a124b910a9c12964014
ZainShabbir <zainansari08@gmail.com>
2020-10-02 20:25:28 +0500

    Add HTML to Login Page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,22 @@
-export default Login() {
+import React from "react";
+
+export default function Login() {
   return (
     <div>
-    <form></form>
+      <form>
+        <div>
+          <label>Phone, email or username</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" />
+        </div>
+        <button type="submit">log in</button>
+      </form>
+      <div>
+        <a href="/signup">Forgot password? · Sign up for Twitter</a>
+      </div>
     </div>
-  )
+  );
 }
