23a5af2ab923b0d9cd0d955eb32f94c89788e1c2
zainsci <zainansari08@gmail.com>
2020-10-03 17:53:54 +0500

    Add Signup to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Signup />
     </div>
   );
 }
