4700cc407735baebe56ea8aeb7229c6b19dbf97c
zainsci <zainansari08@gmail.com>
2020-10-03 19:48:48 +0500

    Add Helper Classes to Signup Page

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -9,7 +9,7 @@ for (let i = 1900; i < 2021; i++) {
   years.push(<option>{i}</option>);
 }
 
-function Signup() {
+export default function Signup() {
   return (
     <div className="signup-popup">
       <div className="auth-page">
@@ -86,7 +86,7 @@ function Signup() {
   );
 }
 
-export default function Signup02() {
+function Signup02() {
   return (
     <div className="signup-popup">
       <div className="auth-page">
@@ -113,28 +113,28 @@ export default function Signup02() {
           </g>
         </svg>
         <button className="btn btn-primary signup-next">Next</button>
-        <div className="signup-ce02">
+        <div className="d-flex flex-col">
           <h2 className="ca-heading">Customize your experience</h2>
 
-          <div className="tos-text">
-            <h3>
+          <div className="mt-40 mb-20">
+            <h3 className="mb-20">
               <strong>
                 Track where you see Twitter content accross the web
               </strong>
             </h3>
-            <div className="signup-ced">
-              <p>
+            <div className="d-flex ">
+              <p className="mr-20">
                 Twitter uses this data to personalize your experience. This web
                 browsing history will never be stored with your name, email, or
                 phone number.
               </p>
-              <input type="checkbox" />
-              <div>
-                For more details about these settings, visit the{" "}
-                <a href="https://help.twitter.com/managing-your-account/new-account-settings">
-                  Help Center
-                </a>
-              </div>
+              <input type="checkbox" className="mr-20" />
+            </div>
+            <div className="mt-20">
+              For more details about these settings, visit the{" "}
+              <a href="https://help.twitter.com/managing-your-account/new-account-settings">
+                Help Center
+              </a>
             </div>
           </div>
         </div>
@@ -147,6 +147,17 @@ function Signup03() {
   return (
     <div className="signup-popup">
       <div className="auth-page">
+        <svg
+          viewBox="0 0 24 24"
+          width="24px"
+          height="24px"
+          className="btn btn-link signup-back"
+          fill="#1da1f2"
+        >
+          <g>
+            <path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path>
+          </g>
+        </svg>
         <strong>Step 3 / 5</strong>
         <button className="btn btn-primary signup-next">Next</button>
         <div className="form-container">
@@ -174,7 +185,9 @@ function Signup03() {
                 provided · <a href="/privacy-policy">Privacy Options</a>
               </div>
             </div>
-            <button className="btn btn-primary sbtn03">Sign up</button>
+            <button className="btn btn-primary w-100 mt-20 py-20">
+              Sign up
+            </button>
           </form>
         </div>
       </div>
