677b91c751f7b4cfd2cb1483c50010d8dfcd8b04
zainsci <zainansari08@gmail.com>
2020-10-03 18:44:14 +0500

    Add HTML Skelton to Signup Page

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -11,63 +11,77 @@ for (let i = 1900; i < 2021; i++) {
 
 export default function Signup() {
   return (
-    <div className="signup">
-      <svg
-        viewBox="0 0 24 24"
-        className="twitter-svg-logo"
-        fill="#1da1f2"
-        width="24px"
-        height="24px"
-      >
-        <g>
-          <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
-        </g>
-      </svg>
-      <button className="btn">Next</button>
-      <form>
-        <div className="auth-label">
-          <label for="signup-name">Name</label>
-          <input type="text" id="signup-name" />
+    <div className="signup-popup">
+      <div className="auth-page">
+        <svg
+          viewBox="0 0 24 24"
+          className="twitter-svg-logo"
+          fill="#1da1f2"
+          width="26px"
+          height="26px"
+        >
+          <g>
+            <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
+          </g>
+        </svg>
+        <button className="btn btn-primary signup-next">Next</button>
+        <div className="form-container">
+          <form className="auth-form">
+            <h2 className="ca-heading">Create your account</h2>
+            <div className="auth-label">
+              <label for="signup-name">Name</label>
+              <input type="text" id="signup-name" />
+            </div>
+            <div className="word-counter">0/50</div>
+            <div className="auth-label">
+              <label for="signup-phone">Phone</label>
+              <input type="text" id="signup-phone" />
+            </div>
+            <a href="/change-to-email">Use email instead</a>
+            <div className="dob-info">
+              <strong>Date of birth</strong>
+              <p>
+                This will not be shown publicly. Confirm your own age, even if
+                this account is for a business, a pet, or somthing else.
+              </p>
+            </div>
+            <div className="date-of-birth">
+              <div className="auth-label dob-c1">
+                <label>Month</label>
+                <select>
+                  <option></option>
+                  <option>January</option>
+                  <option>February</option>
+                  <option>March</option>
+                  <option>April</option>
+                  <option>May</option>
+                  <option>June</option>
+                  <option>July</option>
+                  <option>August</option>
+                  <option>September</option>
+                  <option>October</option>
+                  <option>November</option>
+                  <option>December</option>
+                </select>
+              </div>
+              <div className="auth-label dob-c2">
+                <label>Day</label>
+                <select>
+                  <option></option>
+                  {days}
+                </select>
+              </div>
+              <div className="auth-label dob-c3">
+                <label>Year</label>
+                <select>
+                  <option></option>
+                  {years.reverse()}
+                </select>
+              </div>
+            </div>
+          </form>
         </div>
-        <div className="word-counter">0/50</div>
-        <div className="auth-label">
-          <label for="signup-phone">Phone</label>
-          <input type="text" id="signup-phone" />
-        </div>
-        <a href="/change-to-email">Use email instead</a>
-        <strong>Date Of Birth</strong>
-        <small>
-          This will not be shown publicly. Confirm your own age, even if this
-          account is for a business, a pet, or somthing else.
-        </small>
-        <div className="date-of-birth">
-          <div className="auth-label">
-            <label>Month</label>
-            <select>
-              <option>January</option>
-              <option>February</option>
-              <option>March</option>
-              <option>April</option>
-              <option>May</option>
-              <option>June</option>
-              <option>July</option>
-              <option>August</option>
-              <option>September</option>
-              <option>October</option>
-              <option>November</option>
-              <option>December</option>
-            </select>
-          </div>
-          <div className="auth-label">
-            <label>Day</label>
-            <select>{days}</select>
-          </div>
-          <div className="auth-label">
-            <label>Year</label>
-            <select>{years.reverse()}</select>
-          </div>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
