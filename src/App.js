35075d845c3d87b211e1e4ea602c0789d4df3bbd
zainsci <zainansari08@gmail.com>
2020-10-04 18:12:31 +0500

    Add PublicHome To App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 import "./CSS/Helper.css";
-import Signup from "./Pages/Signup";
+import PublicHome from "./Pages/PublicHome";
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <PublicHome></PublicHome>
     </div>
   );
 }
