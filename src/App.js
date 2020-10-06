4b98568b91ab0d3982a945b313193b1fb99dc505
zainsci <zainansari08@gmail.com>
2020-10-06 19:25:10 +0500

    Add Router To App For Signup and LandingPage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from "react";
 import "./App.css";
 import "./CSS/Helper.css";
 import LandingPage from "./Pages/LandingPage";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage></LandingPage>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/signup">
+            <LandingPage signupWin={true}></LandingPage>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
