00937400e0f59067aa239f9a14a7c4d73bc64838
ZainShabbir <zainansari08@gmail.com>
2020-10-06 22:08:40 +0500

    Add Base For Twitter Feed

---
+++ src/Pages/Base.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+
+class Base extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
