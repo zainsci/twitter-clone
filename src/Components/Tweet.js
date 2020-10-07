66afdd521984e258195a9e902f634bb56afb9c88
zainsci <zainansari08@gmail.com>
2020-10-07 20:03:14 +0500

    Init Tweet Component

---
+++ src/Components/Tweet.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class Tweet extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div className="card">
+          <div className="card__header">
+            <img src="#"></img>
+          </div>
+          <div className="card__body">
+            <div className="card__body__meta">
+              <span className="cbm__name">Aristotle</span>
+              <span className="cbm__username">@Aristotle</span>
+              <span className="cbm__time">1 hour ago</span>
+            </div>
+            <div className="card__body__content">
+              <p>
+                It is the mark of an educated mind to be able to entertain a
+                thought without accepting it.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
