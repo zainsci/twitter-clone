9da505efc522b13cd21815baeb0dd6d071054553
zainsci <zainansari08@gmail.com>
2020-10-07 19:36:05 +0500

    Initialize Component in Explore

---
+++ src/Pages/Explore.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+class Explore extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Hello From Twitter 01</h1>
+        </div>
+        <div>
+          <h1>Hello From Twitter 02</h1>
+        </div>
+        <div>
+          <h1>Hello From Twitter 03</h1>
+        </div>
+      </div>
+    );
+  }
+}
