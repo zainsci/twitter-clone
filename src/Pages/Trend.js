b895c8ad642eaf119fb346846baee01477ce6777
ZainShabbir <zainansari08@gmail.com>
2020-10-09 22:12:23 +0500

    Create Trend Page

---
+++ src/Pages/Trend.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Tweet from "../Components/Tweet";
+
+class Trend extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Tweet userId="1" />
+          <Tweet userId="2" />
+          <Tweet userId="3" />
+          <Tweet userId="4" />
+          <Tweet userId="5" />
+          <Tweet userId="6" />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default Trend;
