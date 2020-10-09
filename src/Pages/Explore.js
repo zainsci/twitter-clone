a74f24918b949345eabb6a57029d84fe17f37ca0
ZainShabbir <zainansari08@gmail.com>
2020-10-09 22:17:02 +0500

    Add Router to Explore

--- src/Pages/Explore.js
+++ src/Pages/Explore.js
@@ -8,18 +8,20 @@ class Explore extends React.Component {
   }
   render() {
     return (
-      <div className="explore">
-        <div className="explore__header">
-          <h3>What's Happening</h3>
+      <Router>
+        <div className="explore">
+          <div className="explore__header">
+            <h3>What's Happening</h3>
+          </div>
+          <div className="explore__cards">
+            <Tweet postId="1" />
+            <Tweet postId="2" />
+            <Tweet postId="3" />
+            <Tweet postId="4" />
+            <Tweet postId="5" />
+          </div>
         </div>
-        <div className="explore__cards">
-          <Tweet postId="1" />
-          <Tweet postId="2" />
-          <Tweet postId="3" />
-          <Tweet postId="4" />
-          <Tweet postId="5" />
-        </div>
-      </div>
+      </Router>
     );
   }
 }
