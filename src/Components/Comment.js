9e8fe36cd9ceeaf0d1c94793623ad390a1a2fc53
ZainShabbir <zainansari08@gmail.com>
2020-10-10 14:13:47 +0500

    Create Comment Component

---
+++ src/Components/Comment.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
