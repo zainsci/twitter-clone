7c829565f160bdf4208da6f61624019f691313ea
ZainShabbir <zainansari08@gmail.com>
2020-10-10 14:12:18 +0500

    Create User Page Component

---
+++ src/Pages/User.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
