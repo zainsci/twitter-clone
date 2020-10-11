6c14d4faf94448b6e86d04ce76a13aaa019cdf5e
zainsci <zainansari08@gmail.com>
2020-10-11 17:37:52 +0500

    Add Fetch Data For Tweet

--- src/Components/Thread.js
+++ src/Components/Thread.js
@@ -4,13 +4,25 @@ import Tweet from "./Tweet";
 class Thread extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isDataLoaded: false,
+      tweetData: {},
+    };
+  }
+  componentDidMount() {
+    fetch("https://randomuser.me/api/")
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({ tweetData: data.results[0], isDataLoaded: true })
+      );
   }
   render() {
     return (
-      <div>
-        <Tweet />
-      </div>
+      <>
+        {this.state.isDataLoaded ? (
+          <Tweet tweetData={this.state.tweetData} isThread={true} />
+        ) : null}
+      </>
     );
   }
 }
