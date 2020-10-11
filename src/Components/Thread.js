315cd64d51f6ff43485c02c152bf25c0d60dac3a
zainsci <zainansari08@gmail.com>
2020-10-11 17:38:21 +0500

    Add Loading Circle for Thread

--- src/Components/Thread.js
+++ src/Components/Thread.js
@@ -21,7 +21,16 @@ class Thread extends React.Component {
       <>
         {this.state.isDataLoaded ? (
           <Tweet tweetData={this.state.tweetData} isThread={true} />
-        ) : null}
+        ) : (
+          <div style={{ width: "100%", display: "flex" }}>
+            <img
+              src="https://abs.twimg.com/a/1496350504/img/t1/Spinner-Dots-30fps-200px.gif"
+              width="80px"
+              height="80px"
+              style={{ margin: "auto" }}
+            />
+          </div>
+        )}
       </>
     );
   }
