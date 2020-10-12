b31dfb584cb61dd224fed9a1c6ebb1e5a0895dfc
zainsci <zainansari08@gmail.com>
2020-10-12 17:12:16 +0500

    Add loading Image to Trend Page

--- src/Pages/Trend.js
+++ src/Pages/Trend.js
@@ -20,16 +20,25 @@ class Trend extends React.Component {
   render() {
     return (
       <div className="trend">
-        {this.state.dataLoaded
-          ? this.state.tweetData.map((tweet) => (
-              <Link
-                to={`/${tweet.login.username}/status/${tweet.login.salt}`}
-                key={tweet.login.uuid}
-              >
-                <Tweet tweetData={tweet} />
-              </Link>
-            ))
-          : null}
+        {this.state.dataLoaded ? (
+          this.state.tweetData.map((tweet) => (
+            <Link
+              to={`/${tweet.login.username}/status/${tweet.login.salt}`}
+              key={tweet.login.uuid}
+            >
+              <Tweet tweetData={tweet} />
+            </Link>
+          ))
+        ) : (
+          <div style={{ width: "100%", display: "flex" }}>
+            <img
+              src="https://abs.twimg.com/a/1496350504/img/t1/Spinner-Dots-30fps-200px.gif"
+              width="80px"
+              height="80px"
+              style={{ margin: "auto" }}
+            />
+          </div>
+        )}
       </div>
     );
   }
