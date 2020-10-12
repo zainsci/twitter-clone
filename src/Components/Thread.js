a6d7882430d79d7a9dec63da59316d6b2889376c
zainsci <zainansari08@gmail.com>
2020-10-12 19:32:26 +0500

    Fix Loading Gif CSS Bug

--- src/Components/Thread.js
+++ src/Components/Thread.js
@@ -22,7 +22,7 @@ class Thread extends React.Component {
         {this.state.isDataLoaded ? (
           <Tweet tweetData={this.state.tweetData} isThread={true} />
         ) : (
-          <div style={{ width: "100%", display: "flex" }}>
+          <div style={{ width: "80px", margin: "auto" }}>
             <img
               src="https://abs.twimg.com/a/1496350504/img/t1/Spinner-Dots-30fps-200px.gif"
               width="80px"
