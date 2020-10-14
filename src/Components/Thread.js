a2802e8be0294e5e7f9900729573403a585f5d41
ZainShabbir <zainansari08@gmail.com>
2020-10-14 21:39:41 +0500

    Convert Thread to Functiona Component

--- src/Components/Thread.js
+++ src/Components/Thread.js
@@ -1,38 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Tweet from "./Tweet";
 
-class Thread extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isDataLoaded: false,
-      tweetData: {},
-    };
-  }
-  componentDidMount() {
+function Thread() {
+  const [isDataLoaded, setDataLoaded] = useState(false);
+  const [tweetData, setTweetData] = useState({});
+
+  useEffect(() => {
     fetch("https://randomuser.me/api/")
       .then((res) => res.json())
       .then((data) =>
         this.setState({ tweetData: data.results[0], isDataLoaded: true })
       );
-  }
-  render() {
-    return (
-      <>
-        {this.state.isDataLoaded ? (
-          <Tweet tweetData={this.state.tweetData} isThread={true} />
-        ) : (
-          <div style={{ width: "80px", margin: "auto" }}>
-            <img
-              src="https://abs.twimg.com/a/1496350504/img/t1/Spinner-Dots-30fps-200px.gif"
-              width="80px"
-              height="80px"
-              style={{ margin: "auto" }}
-            />
-          </div>
-        )}
-      </>
-    );
-  }
+  });
+  return (
+    <>
+      {this.state.isDataLoaded ? (
+        <Tweet tweetData={this.state.tweetData} isThread={true} />
+      ) : (
+        <div style={{ width: "80px", margin: "auto" }}>
+          <img
+            src="https://abs.twimg.com/a/1496350504/img/t1/Spinner-Dots-30fps-200px.gif"
+            width="80px"
+            height="80px"
+            style={{ margin: "auto" }}
+          />
+        </div>
+      )}
+    </>
+  );
 }
 export default Thread;
