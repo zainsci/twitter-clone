71c23bf37871d11d5ff598380e04a8d8b8ee1d1b
zainsci <zainansari08@gmail.com>
2020-10-14 19:26:44 +0500

    Add Fetch Data to User JSX

--- src/Pages/User.js
+++ src/Pages/User.js
@@ -25,25 +25,23 @@ function User() {
           <div className="propfile">
             <div className="profile__header">
               <div className="profile__backimg">
-                <img src={this.state.userData.headerImg} />
+                <img src={userData.headerImg} />
               </div>
             </div>
             <div className="profile__body">
               <div className="profile__img">
-                <img src={this.state.userData.profileImg} />
+                <img src={userData.profileImg} />
               </div>
               <div className="profile__follow">
                 <button className="btn btn-secondary btn-border">Follow</button>
               </div>
               <div className="profile__meta">
                 <div className="profile__name">
-                  <h3>{this.state.userData.name}</h3>
+                  <h3>{userData.name}</h3>
                 </div>
-                <div className="profile__username">
-                  @{this.state.userData.username}
-                </div>
-                <div className="profile__desc mt-10 mb-10">
-                  {this.state.userData.description}
+                <div className="profile__username">@{userData.username}</div>
+                <div className="profile__desc mt-10 mb-10 pr-10">
+                  {userData.description}
                 </div>
                 <div className="profile__detail">
                   <svg
@@ -57,9 +55,7 @@ function User() {
                       <path d="M7.27 22.054c-1.61 0-3.197-.735-4.225-2.125-.832-1.127-1.176-2.51-.968-3.894s.943-2.605 2.07-3.438l1.478-1.094c.334-.245.805-.175 1.05.158s.177.804-.157 1.05l-1.48 1.095c-.803.593-1.326 1.464-1.475 2.45-.148.99.097 1.975.69 2.778 1.225 1.657 3.57 2.01 5.23.785l3.528-2.608c1.658-1.225 2.01-3.57.785-5.23-.498-.674-1.187-1.15-1.992-1.376-.4-.113-.633-.527-.52-.927.112-.4.528-.63.926-.522 1.13.318 2.096.986 2.794 1.932 1.717 2.324 1.224 5.612-1.1 7.33l-3.53 2.608c-.933.693-2.023 1.026-3.105 1.026z"></path>
                     </g>
                   </svg>
-                  <a href={this.state.userData.website}>
-                    {this.state.userData.website}
-                  </a>
+                  <a href={userData.website}>{userData.website}</a>
                   <svg
                     viewBox="0 0 24 24"
                     width="18px"
@@ -78,14 +74,12 @@ function User() {
                       <circle cx="12" cy="17.486" r="1.285"></circle>
                     </g>
                   </svg>
-                  Joined {this.state.userData.joined}
+                  Joined {userData.joined}
                 </div>
                 <div className="profile__numbers">
-                  <strong>{this.state.userData.following}</strong>{" "}
-                  <span>Following</span>
+                  <strong>{userData.following}</strong> <span>Following</span>
                   {" • "}
-                  <strong>{this.state.userData.followers}</strong>{" "}
-                  <span>Followers</span>
+                  <strong>{userData.followers}</strong> <span>Followers</span>
                 </div>
               </div>
             </div>
