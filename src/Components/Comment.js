261687740845b3b526a518d14ac0f157657501ca
zainsci <zainansari08@gmail.com>
2020-10-15 19:31:25 +0500

    Add Loading Image to Comment

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -19,9 +19,19 @@ class Comment extends React.Component {
   render() {
     return (
       <div>
-        {this.state.dataLoaded
-          ? this.state.comments.map((comment) => <Tweet tweetData={comment} />)
-          : null}
+        {this.state.dataLoaded ? (
+          this.state.comments.map((comment) => <Tweet tweetData={comment} />)
+        ) : (
+          <div style={{ width: "80px", margin: "auto" }}>
+            <img
+              src="https://abs.twimg.com/a/1496350504/img/t1/Spinner-Dots-30fps-200px.gif"
+              width="80px"
+              height="80px"
+              style={{ margin: "auto" }}
+              alt="Loading"
+            />
+          </div>
+        )}
       </div>
     );
   }
