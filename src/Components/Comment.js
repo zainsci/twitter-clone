b803d86d1730efb9ed00e2050f1a51459b0dc494
zainsci <zainansari08@gmail.com>
2020-10-15 19:08:43 +0500

    Add Fetch Data to Comment and Tweet

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import Tweet from "./Tweet";
 
 class Comment extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      comments: [],
+      dataLoaded: false,
+    };
+  }
+  componentDidMount() {
+    fetch(
+      "https://raw.githubusercontent.com/zainsci/twitter-clone/master/public/Data/tweets.json"
+    )
+      .then((res) => res.json())
+      .then((data) => this.setState({ comments: data, dataLoaded: true }));
   }
   render() {
     return (
       <div>
-        <div></div>
+        {this.state.dataLoaded
+          ? this.state.comments.map((comment) => <Tweet tweetData={comment} />)
+          : null}
       </div>
     );
   }
 }
+export default Comment;
