9b82a9d565e2de2694563529091c969cb308892b
ZainShabbir <zainansari08@gmail.com>
2020-10-16 18:54:01 +0500

    Creating Loading Spinner

---
+++ src/Components/Loading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Laoding() {
+  return (
+    <div style={{ width: "80px", margin: "auto" }}>
+      <img
+        src="https://abs.twimg.com/a/1496350504/img/t1/Spinner-Dots-30fps-200px.gif"
+        width="80px"
+        height="80px"
+        style={{ margin: "auto" }}
+        alt="Loading"
+      />
+    </div>
+  );
+}
